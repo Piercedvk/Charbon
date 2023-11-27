f7ef2914c013ce4c20573864d035c168cc678632
Piercedvk <150814601+Piercedvk@users.noreply.github.com>
2023-11-27 09:25:25 -0500

    Update MainActivity.java

--- app/src/main/java/com/group9/charbon/MainActivity.java
+++ app/src/main/java/com/group9/charbon/MainActivity.java
@@ -10,5 +10,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+         Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        // Set toolbar title
+        getSupportActionBar().setTitle("D&D Character Creator");
     }
 }
